68e64ea11ebf13b6d2d743ad9871af2382691759
Nicolly Lima <nicollysenai29@gmail.com>
2024-08-30 15:11:58 -0300

    Quebra de linha

--- testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
@@ -4,10 +4,15 @@ describe('Formulário cadastro', () => {
     })
     it('Usuário deve conseguir se cadastrar com sucesso', () => {
     cy.getByData('botao-cadastro').click() /* testaremos agora o botão 'cadastrar' */
+
     cy.getByData('nome-input').type('julio')
+    
     cy.getByData('email-input').type('julio@email.com')
+    
     cy.getByData('senha-input').type('456789')
+    
     cy.getByData('botao-enviar').click()
+    
     cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
     
     })
